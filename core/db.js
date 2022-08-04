cdbf7b98552248023e732ef5929a96923d6be980
Subham0529 <samantasubham9804@gmail.com>
2022-08-04 11:53:57 +0530

    Update Time for inc, activity and eva board 04-08-2022

--- core/db.js
+++ core/db.js
@@ -1,22 +1,22 @@
 const mysql = require('mysql');
 
 // LOCAL
-const db = mysql.createPool({
-    connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'verm'
-});
-
-// SERVER
 // const db = mysql.createPool({
 //     connectionLimit: 10,
 //     host: 'localhost',
-//     user: 'er-360-user',
-//     password: 'Er-360-user@123',
-//     database: 'er-360'
+//     user: 'root',
+//     password: '',
+//     database: 'verm'
 // });
+
+// SERVER
+const db = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'er-360-user',
+    password: 'Er-360-user@123',
+    database: 'er-360'
+});
 
 db.getConnection((err, connection) => {
     if (err) console.log(err);
